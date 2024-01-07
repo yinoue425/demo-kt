fd8071db9f5ce6464f0875da838d2113c661f5f7
yinoue <usuke.i.22@gmail.com>
2024-01-07 21:22:50 +0900

    initial commit

---
+++ src/main/kotlin/com/example/demokt/DemoKtApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demokt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoKtApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoKtApplication>(*args)
+}
